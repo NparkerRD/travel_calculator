e75646b583368f00ec12a0b5c5402af23d71e199
unknown <nicole@rocketdaddy.ai>
2023-11-28 14:33:00 -0500

    displaying results in 'Flying' tab

--- script2.js
+++ script2.js
@@ -1,39 +1,13 @@
 "use strict";
 // Calculator functionality
-const base_hotel = 180;
-const base_per_diem = 35;
+const hotel_per_night = 180;
+const per_diem_per_day = 35;
+const hourly_pay = 120;
 
+// Simple functions
 const roundUp100 = (num) => Math.ceil(num / 100) * 100; // Round to nearest 100
 const roundUp10 = (num) => Math.ceil(num / 10) * 10; // Round to nearest 10
-
-// Create a function for calculating flying
-const calc_flying = function(people, days, nights, hours, cases) {
-    const base_ticket = 1000; // Price for roundtrip ticket
-    const cancellation_fee = 150;
-    const base_ship = 100; // Price for shipping a case of tools (roundtrip)
-    const base_car_rental = 100; // Price per day
-    const base_hourly = 120; // Pay per hour of travel
-
-    const travel_time = (base_hourly * hours) * people;
-    const tickets = base_ticket * people;
-    const flex = cancellation_fee * people; // Flexibility fee
-    const shipping = base_ship * cases;
-
-    const total_flight = roundUp100(travel_time) + tickets + flex + shipping;
-    // console.log(`Total Flight Cost: ${total_flight}`);
-
-    const car_rental = base_car_rental * days;
-
-    const hotel = (base_hotel * nights) * people;
-    const food = (base_per_diem * days) * people;
-
-    const total = total_flight + car_rental + hotel + food;
-    return total;
-};
-
-// const flight = calc_flying(2, 3, 2, 8, 2);
-// console.log(`When flying, the cost comes out to be about: ${flight}`);
-
+const updateText = (el, content) => el.textContent = content.toLocaleString('en-us');
 
 
 // Create a function for calculating driving 
@@ -43,8 +17,8 @@ const calc_driving = function(people, days, nights, hours, distance){
 
     const gas = base_mileage * distance; // Note: Distance should be roundtrip
     const travel_time = (base_hourly * hours) * people;
-    const hotel = (base_hotel * nights) * people;
-    const food = (base_per_diem * days) * people;
+    const hotel = (hotel_per_night * nights) * people;
+    const food = (per_diem_per_day * days) * people;
 
     const total = gas + roundUp10(travel_time) + hotel + food;
     return roundUp10(total);
@@ -56,20 +30,6 @@ const calc_driving = function(people, days, nights, hours, distance){
 
 
 // Page navigation
-/*
-TASKS:
-    * Get response for tab click
-    * Get input values in console on calculate click
-    * Get results in console on calculate click
-    * Get results on page on calculate click
-    * Create error handling for input not entered
-    * Create driving tab view
-    * 
-LATER:
-    * Add commas to numbers
-    * Add clear function/button
-
-*/
 // Get buttons from document
 
 const tabs = document.querySelectorAll(".calc_tab");
@@ -82,39 +42,76 @@ tabs.forEach(tab => {
     })
 });
 
-// inputs.forEach(input => {
-//     console.log(input.id);
-// })
 
-// const getResults = function () {
-//     const inputs = document.querySelectorAll(".input");
+// Function to give individual results
+// Return individiaul values?
+const calc_flying2 = function(days, nights, people, hours, cases) {
+    const ticket_cost = 1000 + 150; // Roundtrip price for ticket + cancellation fee
+    const ship_per_case = 100; // Cost of shipping ONE case of tools
+    const car_per_day = 100; // Cost of renting a car per day
 
-//     inputs.forEach(input => {
-//         console.log(input.id, +input.value);
-//     });
-// }
+    const hotel = hotel_per_night * nights;
+    const food = per_diem_per_day * days;
+    const ticket = ticket_cost;
+    const travel_time = roundUp100(hourly_pay * hours);
+    const shipping = ship_per_case * cases;
+    const car_rental = car_per_day * days;
 
-const getResults = function () {
-    const inputDays = document.querySelector('#inputDays');
-    const inputNights = document.querySelector('#inputNights');
-    const inputPeople = document.querySelector('#inputPeople');
-    const inputHours = document.querySelector('#inputHours');
-    const inputCases = document.querySelector('#inputCases');
-
-    const total = calc_flying(+inputDays.value, +inputNights.value, +inputPeople.value, +inputHours.value, +inputCases.value);
-    console.log(total);
-
-    // Display total
-    document.querySelector('#resultTotalCost').textContent = total;
-
-    console.log(base_hotel * +inputNights.value);
-    document.querySelector('#resultHotel').textContent = base_hotel * +inputNights.value;
-    // document.querySelector('#resultTest').textContent = base_hotel * +inputNights.value;
+    const total = ((hotel + food + ticket + travel_time) * people) + shipping + car_rental;
+    return [total, hotel, food, ticket, travel_time, ship_per_case, car_per_day];
+    
 }
 
+// To be used with the test/new function calc_flying2
+// Testing out destructuring
+const getResults2 = function () {
+    // Getting input elements
+    const [inputDays, 
+           inputNights,
+           inputPeople, 
+           inputHours, 
+           inputCases] = document.querySelectorAll(".input");
+
+    // Getting result elements 
+    const [resultTotal, 
+           resultHotel,
+           resultFood, 
+           resultTickets, 
+           resultTravelTime, 
+           resultShipping, 
+           resultCarRental] = document.querySelectorAll(".result");
+    
+    const [total, hotel, food, ticket, travelTime, shipping, carRental] = calc_flying2(+inputDays.value, +inputNights.value, +inputPeople.value, +inputHours.value, +inputCases.value);
+    
+    updateText(resultTotal, total);
+    updateText(resultHotel, hotel);
+    updateText(resultFood, food);
+    updateText(resultTickets, ticket);
+    updateText(resultTravelTime, travelTime);
+    updateText(resultShipping, shipping);
+    updateText(resultCarRental, carRental);
+    
+}
 
 // Get input values in console on calculate click
 btnCalculate.addEventListener("click", (e) => {
     e.preventDefault();
-    getResults();
+    getResults2();
 })
+
+
+/*
+TASKS:
+    * Get response for tab click
+    * Get input values in console on calculate click
+    * Get results in console on calculate click
+    * Get results on page on calculate click
+    * Create error handling for input not entered
+    * Create driving tab view
+    * 
+LATER:
+    * Add commas to numbers
+    * Add clear function/button
+    * Make sure 'People' and 'Hours' are always valid; don't allow 0
+
+*/
